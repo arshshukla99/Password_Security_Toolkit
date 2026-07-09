1f75f356a33d74c174a542ec86e53b5989f27de2
Arsh <arshshukla2487@gmail.com>
2026-07-09 16:14:39 +0530

    Enhance password security checks and versioning
    Updated version and improved password audit logic to check for common dictionary words.

--- password_security_toolkit.py
+++ password_security_toolkit.py
@@ -1,7 +1,6 @@
-print("\n=== Password Security Toolkit ===\n")
+print("\n=== Password Security Toolkit v0.3===\n")
 print("Welcome to Password Security Toolkit!")
 print("A Strong Password can be a reason why your account will not be Hijacked in Future...\n")
-
 password = input('Enter Your Password Here for an Audit: ')
 print()
 
@@ -9,7 +8,7 @@ MIN_LENGTH = 8
 MAX_LENGTH = 12
 MAX_SCORE = 5
 
-#Function that checks the password and scores it according to its Length and what characters are involved in it.
+#pass_check() function that checks the password and scores it according to its Length and what characters are involved in it.
 def pass_check(password):
     upper = 0
     digit = 0
@@ -44,7 +43,29 @@ def pass_check(password):
     
     return upper, digit, special, score, space
 
-#Function that takes the password and it characteristics and gives suggestions to improve the password. 
+#dict_check() check is the password or a particular word in the password is in the wordlist or not and yields True/False accordingly.
+dict_word_found = False
+dict_pass_found = False
+word_found = ""
+
+def dict_check(password):
+    temp_word_found = False
+    temp_pass_found = False
+    temp_word = ''
+    
+    with open("common_passwords.txt", "r") as f:
+        wordlist = f.read().rstrip("\n").splitlines()
+        for i in wordlist:
+            if len(i) >= 3 and i in password.lower():
+                temp_word_found = True
+                temp_word = i
+                break
+            if len(i) >=3 and i == password.lower():
+                temp_pass_found = True
+                break
+        return temp_word_found, temp_word, temp_pass_found
+
+#suggestions() function that takes the password and it characteristics and gives suggestions to improve the password.
 def suggestions(password, upper, digit, special, score, space):
     if space == 0:
         if score == 0 :
@@ -114,8 +135,22 @@ def suggestions(password, upper, digit, special, score, space):
 # takes the arguments returned from the function pass_check() and store it in variable
 upper, digit, special, score, space = pass_check(password)
 
-#suggestions() function takes those arguments as parameters
-suggestions(password, upper, digit, special, score, space)
+#the variables unpack the ouput of dict_check()
+dict_word_found, word_found, dict_pass_found = dict_check(password)
+
+#Checks if the password or particular word in the password found or not 
+if dict_pass_found == False:
+    if dict_word_found == True:
+        suggestions(password, upper, digit, special, score, space)
+
+        print("\n⚠ Dictionary Word Detected!\n")
+        print(f"Found word : '{word_found}' in Common Attack Dictionary\n")
+        print(f"It is recommended not to use common words in your password which can be found in Common Attack Dictionaries...")
+    else:
+        upper, digit, special, score, space = pass_check(password)
+        suggestions(password, upper, digit, special, score, space)
+else:
+    print(f"Your Password is too Common!\nFound in Common Attack Dictionary.\n\nYour Password Scored 0/{MAX_SCORE} in terms of Security.")
 
 #Choice for the User if he wants continuation on the password Audit
 while True:
@@ -126,11 +161,24 @@ while True:
         print()
         password = retry_pass
         upper, digit, special, score, space = pass_check(password)
+
+        dict_word_found, word_found, dict_pass_found = dict_check(password)
         
-        suggestions(password, upper, digit, special, score, space)
+        if dict_pass_found == False:
+            if dict_word_found == True:
+                suggestions(password, upper, digit, special, score, space)
+                    
+                print("\n⚠ Dictionary Word Detected!\n")
+                print(f"Found word : '{word_found}' in Common Attack Dictionary\n")
+                print(f"It is recommended not to use common words in your password which can be found in Common Attack Dictionaries...")
+            else:
+                upper, digit, special, score, space = pass_check(password)
+                suggestions(password, upper, digit, special, score, space)
+        else:
+           print(f"Your Password is too Common!\nFound in Common Attack Dictionary.\n\nYour Password Scored 0/{MAX_SCORE} in terms of Security.")
+                
     elif choice.lower() == 'n':
         break
 
     else:
         print("Invalid Choice! Give Choice inputting single characters like 'y' or 'Y' or 'n' or 'N' ")
-        

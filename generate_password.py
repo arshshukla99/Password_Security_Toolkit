d2e6bd7c997ae512a4dae63a28e58985776003db
Arsh <arshshukla2487@gmail.com>
2026-07-25 23:57:09 +0530

    Add password generation script
    This script generates a random password based on user-defined length, ensuring it is between 8 and 16 characters.

---
+++ generate_password.py
@@ -0,0 +1,25 @@
+import string
+import secrets
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+digit = string.digits
+unichar = string.punctuation
+
+allchar = upper + lower + digit + unichar
+
+while True:
+
+    choice = int(input("Input the no. of characters of the password you want : "))
+
+    if choice < 8 or choice > 16:
+        print("The Password should be in between 8 to 16 characters :/")
+
+    elif choice >= 8 or choice <= 16:
+        password = ''
+        for i in range(choice):
+            random = secrets.choice(allchar)
+            password += random  
+
+        print(password)
+        break

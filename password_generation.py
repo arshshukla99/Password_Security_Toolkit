97edd81b1799cff4b55d515ca20a801efbc35075
Arsh <arshshukla2487@gmail.com>
2026-07-27 23:39:47 +0530

    Enhance password generation tool output
    Added formatting and user prompts for better interaction.

--- password_generation.py
+++ password_generation.py
@@ -1,6 +1,10 @@
 import string
 import secrets
 
+print('\n'+"="*100)
+print(" "*35 +"PASSWORD GENERATION TOOL")
+print("="*100 + '\n')
+
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 digit = string.digits
@@ -13,7 +17,7 @@ while True:
     choice = int(input("Input the no. of characters of the password you want : "))
 
     if choice < 8 or choice > 16:
-        print("The Password should be in between 8 to 16 characters :/ \n")
+        print("The Password should be in between 8 to 16 characters :/")
 
     elif choice >= 8 or choice <= 16:
         password = ''
@@ -21,6 +25,7 @@ while True:
         for i in range(choice):
             random = secrets.choice(allchar)
             password += random  
-
-        print(password)
+        
+        print()
+        print("Here is You Secure Password :",password)
         break

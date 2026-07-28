3a207dadb172123c3128de74b475c4a353a2c93e
Arsh <arshshukla2487@gmail.com>
2026-07-28 15:35:31 +0530

    Update Password Security Toolkit to version 0.8

--- password_security_toolkit.py
+++ password_security_toolkit.py
@@ -1,22 +1,30 @@
 import sys
 import subprocess
 
-print("\n=== Password Security Toolkit v0.7 ===\n")
+print("\n=== Password Security Toolkit v0.8 ===\n")
 print("Welcome to Password Security Toolkit!")
 print("A Strong Password can be a reason why your account will not be hijacked in Future...\n")
 
-choice = int(input("Choose what tool to continue with :\n1) Password Audit\n2) Password Generation\n3) Exit\n\nYour Choice : "))
+choice = 0
 
-if choice == 1:
-    subprocess.run([sys.executable,"password_audit.py"])
+while True:
+    try:
+        choice = int(input("Choose what tool to continue with :\n1) Password Audit\n2) Password Generation\n3) Exit\n\nYour Choice : "))
+    except:
+        print("Invalid Choice!! Enter Numbers Only!\n")
+        continue
+        
+    if choice == 1:
+        subprocess.run([sys.executable,"password_audit.py"])
+        
+    elif choice == 2:
+        subprocess.run([sys.executable,"password_generation.py"])
+        
+    elif choice == 3:
+        print("Exiting...")
+        sys.exit()
+    
+    else:
+        print("\nPlease Enter a Valid Choice !\n")
+        
 
-elif choice == 2:
-    subprocess.run([sys.executable,"password_generation.py"])
-
-elif choice == 3:
-    print("Exiting...")
-    sys.exit()
-
-else:
-    print("Please Enter a Valid Choice !")
-    sys.exit()

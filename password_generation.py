7f94c805596427c9af5fab1f02a7062c762f1503
Arsh <arshshukla2487@gmail.com>
2026-08-08 14:12:16 +0530

    Enhance password generation with complexity validation
    Added password complexity checks and integrated password audit.

--- password_generation.py
+++ password_generation.py
@@ -1,5 +1,6 @@
 import string
 import secrets
+from password_audit import pass_audit
 
 print('\n'+"="*150)
 print(" "*60 +"PASSWORD GENERATION TOOL")
@@ -9,29 +10,49 @@ lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 digit = string.digits
 unichar = string.punctuation
+f_lower = False
+f_upper = False
+f_digit = False
+f_unichar = False
 
 allchar = upper + lower + digit + unichar
 choice = 0
 
 while True:
     try:
-        choice = int(input("Input the No. of characters of the password : "))
+        choice = int(input("Input the No. of characters of the password Starting from 8 to 16 : "))
         
         if choice < 8 or choice > 16:
             print("\nThe Password should be in between 8 to 16 characters :/\n")
 
         else:
-            password = ''
+            password = ""
         
             for i in range(choice):
                 random = secrets.choice(allchar)
-                password += random  
-        
-            print('\n'+ '-'*50+'\n')
-            print("Here is You Secure Password :",password,'\n')
-            print('-'*50 +'\n')
-            print("="*150+'\n')
-            break
-        
+                password += random
+
+            for i in lower:
+                if i in password:
+                    f_lower = True
+            for j in upper:
+                if j in password:
+                    f_upper = True
+            for k in digit:
+                if k in password:
+                    f_digit = True
+            for l in unichar:
+                if l in password:
+                    f_unichar = True
+
+            if f_lower and f_upper and f_digit and f_unichar:
+                print('\n'+ '-'*50+'\n')
+                print("Here is You Secure Password :",password,'\n')
+                print('-'*50 +'\n')
+                pass_audit(password)
+                break
+
+            else:
+                continue
     except:
         print("\nError : Enter Numbers Only!\n")
